45594f052a0c7446341214b83857080c670543b8
LordShenron <shen.priyanshu@gmail.com>
2020-02-23 08:36:18 +0000

    X00T: use crossfatch fp from feb patch
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -37,7 +37,7 @@ TARGET_VENDOR_PRODUCT_NAME := X00T
 TARGET_INCLUDE_WIFI_EXT := true
 SUPERIOR_OFFCIAL := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRIVATE_BUILD_DESC="crosshatch-user 10 QQ1A.200105.002 6031801 release-keys"
+        PRIVATE_BUILD_DESC="crosshatch-user 10 QQ1A.200205.002 6084386 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:10/QQ1A.200105.002/6031801:user/release-keys
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:10/QQ1A.200205.002/6084386:user/release-keys
